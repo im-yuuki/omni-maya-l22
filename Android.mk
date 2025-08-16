1b1bb619ac30c3460290ea931f903c1488b9bc21
Yuuki <me@june8th.eu.org>
2025-08-16 21:00:34 +0700

    fix: change codename to maya

--- Android.mk
+++ Android.mk
@@ -7,6 +7,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),maya-l22)
+ifeq ($(TARGET_DEVICE),maya)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/huawei/maya-l22
+DEVICE_PATH := device/huawei/maya
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -43,8 +43,8 @@ BOARD_KERNEL_TAGS_OFFSET := 0x0df88000
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_CONFIG := maya-l22_defconfig
-TARGET_KERNEL_SOURCE := kernel/huawei/maya-l22
+TARGET_KERNEL_CONFIG := maya_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/maya
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
